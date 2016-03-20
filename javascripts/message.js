fbe8782c81d07de5ec81ae6e1198d747acb00d1e
yuhaoHo <howard15195@gmail.com>
2016-03-20 08:39:54 +0800

    final

--- javascripts/message.js
+++ javascripts/message.js
@@ -32,7 +32,55 @@ function MessageBox()
           }, i * 50);
 		}
         timer = setTimeout(function() {
-            _welcomeBox.innerText = welcomeMessage;
+            _welcomeBox.innerText = welcomeMessage + '\n' + '▋';
           }, 6000);
+	}
+
+	_obj.saySomething = function(m) {
+		var originM = m.replace('▋','');
+		var sentence = 'I am going to copy what Yu-Hao wants to say.' + '\n' + 'Ho Yu-Hao:' + '\n' 
+		+ 'Hello, Cherry Wu. HAPPY BIRTHDAY!! I am very glad to make friend with you.' + '\n' 
+		+ 'We have finished a lot of painting!!! That\'s a very interesting memory.';
+		var appear;
+		_countM = 0;
+		console.log(sentence.length);
+		for (var i = 0; i < sentence.length; i+=1) {
+          
+		  timer = setTimeout(function() {
+		  	appear = originM;
+            for (var j = 0; j <= _countM; j+=1)
+          	  appear += sentence[j];
+            appear += '▋';
+            _countM+=1;
+            _welcomeBox.innerText = appear;
+          }, i * 50);
+		}
+		timer = setTimeout(function() {
+            _welcomeBox.innerText = appear.replace('▋','');
+          }, 9700);
+	}
+
+	_obj.saySomethingSecond = function(m) {
+		var originM = m.replace('▋','') + '\n';
+		var sentence = 'We also talk the story about Little Prince. When will the story be ended xD ?' + '\n' 
+		+ 'Please keep your smile not only today but everyday!! It\'s very beautiful thing.' + '\n' 
+		+ 'Anyway, all I want to say is ...';
+		var appear;
+		_countM = 0;
+		console.log(sentence.length);
+		for (var i = 0; i < sentence.length; i+=1) {
+          
+		  timer = setTimeout(function() {
+		  	appear = originM;
+            for (var j = 0; j <= _countM; j+=1)
+          	  appear += sentence[j];
+            appear += '▋';
+            _countM+=1;
+            _welcomeBox.innerText = appear;
+          }, i * 50);
+		}
+		timer = setTimeout(function() {
+            _welcomeBox.innerText = appear.replace('▋','');
+          }, 9700);
 	}
 };
